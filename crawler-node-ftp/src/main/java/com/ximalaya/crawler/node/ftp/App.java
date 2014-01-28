9f8a2c2eda0195ee5c853d066960edc21f72f938
wikimore <wikimore357@gmail.com>
2014-01-28 17:38:11 +0800

    init

---
+++ crawler-node-ftp/src/main/java/com/ximalaya/crawler/node/ftp/App.java
@@ -0,0 +1,13 @@
+package com.ximalaya.crawler.node.ftp;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
